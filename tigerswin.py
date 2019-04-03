fda95aa6e23c894c334b8c55248df206cac99aaa
gfolbe318 <gmfolbe@yahoo.com>
2019-04-02 22:14:30 -0400

    bot working... for now

--- tigerswin.py
+++ tigerswin.py
@@ -61,15 +61,13 @@ month_list = [
     "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"
 ]
 
-# Used to tell the place of the Tigers
-nth = {
-    0 : "1st",
-    1 : "2nd",
-    2 : "3rd",
-    3 : "4th",
-    4 : "5th"
-}
+# List of days for indexing purposes
+days_list = [
+    "Monday", "Tuesday", "Wednesday", "Thursday", 
+    "Friday", "Saturday", "Sunday"
+]
 
+# Used to easily print out "beat/lost to for a single game"
 win_loss_single = {
     "W" : "beat the",
     "W-wo" : "beat the",
@@ -77,6 +75,7 @@ win_loss_single = {
     "L-wo" : "lost to the"
 }
 
+# Used to easily print out "won/lost" for doubleheaders
 win_loss_double = {
     "W" : "won",
     "W-wo" : "won",
@@ -111,15 +110,41 @@ MLB_teams = {
     "SDP" : "Padres", "SFG" : "Giants"
 }
 
+# Print the score out with the greater number always printing first
 def print_score(runs_scored, runs_allowed):
     if runs_scored > runs_allowed:
         return runs_scored + "-" + runs_allowed
     else:
         return runs_allowed + "-" + runs_scored
 
+# If the Tigers didn't play yesteday, print it out. A little confused on how I
+# will handle rain delays. I might have to do something where I keep track of if
+# they played today or not, but I'll cross that bridge when I get there. I don't
+# know how BR handles a rain delay on their website
 def get_result_no_game():
-    return "The Tigers did not play yesterday"
+    return "The Tigers did not play yesterday.\n"
 
+# Print out the score of one game
+def get_result_one_game(game):
+    return(
+        "Yesterday, the Tigers " + win_loss_single[game["result"]] + " " +
+        MLB_teams[game["opponent"]] + " " + 
+        print_score(game["runs-scored"], game["runs-allowed"]) + ".\n"
+
+    )
+
+# Print out the result of a doubleheader
+def get_result_two_games(game1, game2):
+    return(
+        "Yesteday, the Tigers had a doubleheader against the " +
+        MLB_teams[game1["opponent"]] + ".\nThey " + win_loss_double[game1["result"]] +
+        " the first game " + 
+        print_score(game1["runs-scored"], game1["runs-allowed"]) + 
+        ", and " + win_loss_double[game2["result"]] + " the second game " +
+        print_score(game2["runs-scored"], game2["runs-allowed"]) + ".\n"
+    )
+
+# Function to print out standings
 def get_standings(standings):
     games_back = ""
     position = ""
@@ -153,8 +178,8 @@ def get_standings(standings):
                 games_back + " lead.\n"
             )
 
-        # If they are tied with one other team, find out who the other team 
-        # is by looking at the other row
+        # If they are tied with one other team, find out who the other team is
+        # by looking at the other row
         elif num_teams_tied == 2:
             if tigers_row == 0:
                 other_team = standings.at[1, "AL"]
@@ -190,25 +215,33 @@ def get_standings(standings):
             "trailing by " + games_back + " game" + do_I_need_an_s + ".\n"
         ) 
 
+# Get the record and current streak of the Tigers
 def get_record(record, streak):
+    
+    # BR prints out the streak in the form: "-" to represent a 1-game losing
+    # streak "+" to represent a 1-game winning streak and so on and so forth.
+    # Here, I just see if its a W or L streak
     typeStreak = "winning"
     if streak.find('-') != -1:
         typeStreak = "losing"
 
+    # Length of the streak is the amount of +'s or -'s
     num = str(len(streak))
+    
+    # Do I print "a" or "an" when referring to their streak?
     english_length = p.number_to_words(num)
-
+    
+    # p.a(word) returns the correct usage of "a word" or "an word" I parse what
+    # it returns and then only take the "a" or "an" part of it.
     helper = p.a(english_length)
     correctUsage = helper.split(" ")[0]
 
-    dummy_string = (
+    return (
         "Their current record is " + record + ", and they are on " +
         correctUsage + " " + num + " game " + typeStreak + " streak."
     )
 
-    return dummy_string
-
-
+# I'm printing out what happened last night, so I want to get the day before.
 def get_day_before(date):
     orig_month, orig_day = date.split(" ")
     new_day = ""
@@ -225,8 +258,17 @@ def get_day_before(date):
     else:
         new_month = month_list[int(month_to_number[orig_month]) - 2]
         new_day = days_in_month[new_month]
+
+    # Get the correct index of the day of the week. Monday is 0, Tuesday is 1, and so on.
+    index = datetime.date(int(year), int(month), int(day)).weekday()
     
-    return new_month + " " + new_day
+    # The day before monday is sunday. Otherwise, subtract one day
+    if index == 0:
+        index = 6
+    else:
+        index -= 1    
+    weekday = days_list[index]
+    return weekday + ", " + new_month + " " + new_day
 
 # This section of code is used to scrape the homepage of baseball reference and
 # find out the current standings of the AL Central. 
@@ -258,15 +300,18 @@ games_on_date = schedule_df[schedule_df["Date"].str.contains(key)]
 games_on_date = games_on_date.reset_index(drop = True)
 num_games = games_on_date.shape[0]
 
+# Correctly select the information about the game we need from the row
 game = {}
 game["result"] = games_on_date.at[0, "W/L"]
 game["opponent"] = games_on_date.at[0, "Opp"]
 game["runs-scored"] = games_on_date.at[0, "R"]
 game["runs-allowed"] = games_on_date.at[0, "RA"]
 
+# Get the record of the Tigers and their current streak
 record = games_on_date.at[0, "W-L"]
 streak = games_on_date.at[0, "Streak"]
 
+# If there was a double header, get the information of that game too
 game2 = {}
 if num_games == 2:
     game2["result"] = games_on_date.at[1, "W/L"]
@@ -274,18 +319,23 @@ if num_games == 2:
     game2["runs-scored"] = games_on_date.at[1, "R"]
     game2["runs-allowed"] = games_on_date.at[1, "RA"]
     
+    # Overwrite the streak and the record to get the more recent info
     record = games_on_date.at[1, "W-L"]
     streak = games_on_date.at[1, "Streak"]
 
-
-
-
-# We must now create a string of what will be tweeted
-result_line = ""
-
+# We must now create a string of what will be tweeted. Correctly select
+# which function to call based on the number of games.
+if num_games == 0:
+    result_string = get_result_no_game()
+if num_games == 1:
+    result_string = get_result_one_game(game)
+if num_games == 2:
+    result_string = get_result_two_games(game, game2)
 
 standings_line = get_standings(AL_Central_Standings)
-print(standings_line)
-
-
 streak_line = get_record(record, streak)
+
+# Print out our tweet!
+tweet = result_string + "\n" + standings_line + "\n" + streak_line
+
+api.update_status(tweet)
